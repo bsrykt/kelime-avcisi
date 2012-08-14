371d13d14942f224cfd79e9a3d5860997823ff87
Başar Aykut <bsrykt@gmail.com>
2012-08-14 17:52:37 +0300

    Initial commit of the project

---
+++ KelimeAvcisi/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Globalization;
+
+namespace com.bsrykt.KelimeAvcisi
+{
+    public partial class MainWindow : Window
+    {
+        private string letters;
+        private char[] letterArray;
+
+        private Solver s;
+        private static readonly int minWordLength = 3;
+        private static readonly int maxWordLength = 10;
+        private static readonly int matrixSize = 4;
+
+        private static readonly string[] backs = new string[]{"back1", "back2", "back3"};
+        private static readonly string dictionaryPath = "sozluk.txt";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Background = randomBackground();
+
+
+            textLetters.Focus();
+
+            s = new Solver(matrixSize, Locale.TURKISH);
+            int wordCount = s.LoadDictionary(dictionaryPath, minWordLength, maxWordLength);
+
+            this.Title = "Kelime Avcısı @bsrykt | " + wordCount + " adet kelime yüklendi..";
+        }
+
+        private ImageBrush randomBackground()
+        {
+            int random = (new Random()).Next(0,3);
+
+            return new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/KelimeAvcisi;component/res/" + backs[random] + ".jpg")));
+        }
+
+
+        private void textLetters_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            letters = textLetters.Text;
+            letterArray = letters.ToCharArray();
+
+            for (int i = 0; i < matrixSize*matrixSize; i++)
+            {
+                TextBox txt = UIHelper.FindChild<TextBox>(this, "txt" + i);
+                if (null != txt)
+                {
+                    txt.Text = letterArray.Length>i?letterArray[i].ToString():"";
+                }
+            }
+
+            for (int i = minWordLength; i <= maxWordLength; i++)
+            {
+                TextBox tb = UIHelper.FindChild<TextBox>(this, "txtWords" + i);
+
+                if (null != tb)
+                {
+                    tb.Text = "";
+                }
+            }
+            txtWords42.Text = "";
+            txtWords32.Text = "";
+
+            if (letters.Length == 16)
+            {
+                List<KeyValuePair<string,int[]>> wordsFound =  s.Find(letters);
+
+                wordsFound.Sort(compareStringsByLength);
+
+                int[] txtWordsBreak = new int[] { 1,1,1,1,1,1,1,1,1,1,1,1};
+                int word4Count = 0;
+                int word3Count = 0;
+                TextBox tb;
+
+                foreach (KeyValuePair<string, int[]> word in wordsFound)
+                {
+
+                    if (word.Key.Length == 3)
+                    {
+                        word3Count++;
+                    }
+                    if (word.Key.Length == 4)
+                    {
+                        word4Count++;
+                    }
+
+                    if (word.Key.Length == 3 && word3Count > 21)
+                    {
+                        tb = UIHelper.FindChild<TextBox>(this, "txtWords" + word.Key.Length + "2");
+                    }
+                    else if (word.Key.Length == 4 && word4Count > 21)
+                    {
+                        tb = UIHelper.FindChild<TextBox>(this, "txtWords" + word.Key.Length + "2");
+                    }
+                    else
+                    {
+                        tb = UIHelper.FindChild<TextBox>(this, "txtWords" + word.Key.Length);
+                    }
+                    if (null == tb)
+                    {
+                        continue;
+                    }
+                    tb.Text += word.Key + System.Environment.NewLine;
+
+                    int mod = word.Key.Length >= 5 ? 2 : 3;
+
+                    if (txtWordsBreak[word.Key.Length] % mod == 0)
+                    {
+                        tb.Text += System.Environment.NewLine;
+                    }
+                    txtWordsBreak[word.Key.Length]++;
+                }
+            }
+        }
+
+        private int compareStringsByLength(KeyValuePair<string, int[]> a, KeyValuePair<string, int[]> b)
+        {
+            if (a.Key.Length != b.Key.Length)
+            {
+                return b.Key.Length.CompareTo(a.Key.Length);
+            }
+            else
+            {
+                return a.Key.CompareTo(b.Key);
+            }
+        }
+    }
+}
---
+++ KelimeAvcisi/Solver.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Globalization;
+
+namespace com.bsrykt.KelimeAvcisi
+{
+    public sealed class Locale
+    {
+        public static readonly Locale TURKISH = new Locale("tr-TR");
+        public static readonly Locale ENGLISH = new Locale("en-US");
+
+        private readonly string value;
+
+        private Locale (string value)
+	    {
+            this.value = value;
+	    }
+
+        public override string ToString()
+        {
+            return value;
+        }
+
+        public static implicit operator string(Locale locale)
+        {
+            return locale.ToString();
+        }
+    }
+
+    class Solver
+    {
+        private Dictionary<string, List<string>> dictionary = new Dictionary<string, List<string>>();
+        private Dictionary<string, int[]> wordsFound = new Dictionary<string, int[]>();
+        private int[][] neighbors;
+        private string letters;
+        private int matrixSize = 4;
+
+        private Locale locale;
+        public Locale Locale
+        {
+            get { return locale; }
+            set { locale = value; }
+        }
+
+        public Solver(int matrixSize, Locale locale)
+        {
+            this.matrixSize = matrixSize;
+            Locale = locale;
+
+            generateNeighborsTable();
+        }
+
+        public List<KeyValuePair<string, int[]>> Find(string letters)
+        {
+            this.letters = letters.ToLower(new CultureInfo(Locale, false));
+
+            wordsFound = new Dictionary<string,int[]>();
+            string currentLetter;
+            List<string> wordList;
+
+            for (int i = 0; i <= matrixSize*matrixSize-1; i++)
+            {
+                currentLetter = this.letters.Substring(i, 1);
+
+                if (currentLetter.Equals("ğ"))
+                {
+                    continue;
+                }
+
+                if (dictionary.ContainsKey(currentLetter))
+                {
+                    wordList = dictionary[currentLetter.ToString()];
+
+                    foreach (string word in wordList)
+                    {
+                        List<int> wordIndexes;
+                        if (null != (wordIndexes = findWord(i, new List<int>(), word, 0)))
+                        {
+                            if (!wordsFound.ContainsKey(word))
+                            {
+                                wordsFound.Add(word,wordIndexes.ToArray());
+                            }
+                        }
+                    }
+                }
+            }
+
+            return wordsFound.ToList();
+        }
+
+        private List<int> findWord(int currentIndex, List<int> usedIndexes, string word, int wordIndex)
+        {
+            List<int> result = null;
+
+            if (!letters.Substring(currentIndex,1).Equals(word.Substring(wordIndex, 1)))
+            {
+                return null;
+            }
+
+            if (null != result || wordIndex == word.Length - 1)
+            {
+                usedIndexes.Add(currentIndex);
+                return usedIndexes;
+            }
+
+            List<int> usedNew = new List<int>();
+            if (usedIndexes.Count > 0)
+            {
+                foreach (int item in usedIndexes)
+                {
+                    usedNew.Add(item);
+                }
+            }
+            usedNew.Add(currentIndex);
+
+            foreach (int next in neighbors[currentIndex])
+            {
+                if (usedNew.Contains(next))
+                {
+                    continue;
+                }
+
+                result = findWord(next, usedNew, word, wordIndex + 1);
+                if (null != result)
+                {
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private int[] getNeighborIndexes(int index, int matrixSize)
+        {
+            int row = index / matrixSize;
+            int col = index - row * matrixSize;
+            int neighborSize = 1;
+            List<int> neighborsFound = new List<int>((neighborSize * 2 + 1) * (neighborSize * 2 + 1) - 1);
+            int[,] matrix = new int[matrixSize, matrixSize];
+
+            for (int r = 0; r < matrixSize; r++)
+            {
+                for (int c = 0; c < matrixSize; c++)
+                {
+                    matrix[r, c] = r * matrixSize + c;
+                }
+            }
+
+            for (int r = Math.Max(0, row - neighborSize); r <= Math.Min(row + neighborSize, matrixSize - 1); r++)
+            {
+                for (int c = Math.Max(0, col - neighborSize); c <= Math.Min(col + neighborSize, matrixSize - 1); c++)
+                {
+                    if (!(r == row && c == col))
+                    {
+                        neighborsFound.Add(matrix[r, c]);
+                    }
+                }
+            }
+
+            return neighborsFound.ToArray();
+        }
+
+        private void generateNeighborsTable()
+        {
+            neighbors = new int[matrixSize * matrixSize][];
+
+            for (int i = 0; i < matrixSize * matrixSize; i++)
+            {
+                neighbors[i] = getNeighborIndexes(i, matrixSize);
+            }
+        }
+
+        public int LoadDictionary(string filePath, int minWordLength, int maxWordLength)
+        {
+            int wordCount = 0;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    string[] words;
+                    string word;
+                    string firstLetter;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        line = line.Trim();
+
+                        // Ignore empty lines and lines starting with # character
+                        if (line.StartsWith("#") || line.Equals(""))
+                        {
+                            continue;
+                        }
+
+                        words = line.Split(' ');
+                        word = words[0].Trim().ToLower(new CultureInfo(locale.ToString(), false));
+                        firstLetter = word.Substring(0, 1);
+
+                        if (!dictionary.ContainsKey(firstLetter))
+                        {
+                            dictionary.Add(firstLetter, new List<string>());
+                        }
+
+                        if (words[0].Length >= minWordLength && words[0].Length <= maxWordLength)
+                        {
+                            dictionary[firstLetter].Add(words[0]);
+                            wordCount++;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Dictionary could not be read");
+                Console.WriteLine(e.Message);
+            }
+
+            return wordCount;
+        }
+    }
+}
